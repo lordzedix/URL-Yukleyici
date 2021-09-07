8745b71c8fb8679a87977be00ec6c4df7e71b9b6
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-09-07 11:37:32 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,36 +1,32 @@
 class Translation(object):
-    START_TEXT = """Hi {},
-I'm X-URL Uploader!
-You can upload HTTP/HTTPS direct link, Using this bot!
-
-/help for more details!"""
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
-    DOWNLOAD_START = "Now Downloading.."
-    UPLOAD_START = "Now Uploading.."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @xurluploaderbot)"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    START_TEXT = """Merhaba {},
+Ben bir URL Yükleyicisiyim!
+Bu Botu kullanarak HTTP/HTTPS bağlantılarını yükleyebilirsiniz!
+daha fazla bilgi için /help!"""
+    FORMAT_SELECTION = "İstediğiniz formatı seçin: <a href='{}'>dosya boyutu yaklaşık olabilir</a> \nKapak fotoğrafı ayarlamak istiyorsanız, aşağıdaki düğmelerden herhangi birine dokunmadan önce veya hızlı bir şekilde fotoğraf gönderin.\nKapak fotoğrafını silmek için /delthumb kullanabilirsiniz."
+    SET_CUSTOM_USERNAME_PASSWORD = """Premium videoları indirmek istiyorsanız, aşağıdaki formatı sağlayın:
+URL | dosyaadı | kullanıcıadı | şifre"""
+    DOWNLOAD_START = "Şimdi İndiriliyor.."
+    UPLOAD_START = "Şimdi Yükleniyor.."
+    RCHD_TG_API_LIMIT = "{} saniye içinde İndirildi.\nAlgılanan Dosya Boyutu: {}\nÜzgünüm. Ancak, TELEGRAM API sınırlamaları nedeniyle 2GB'DEN büyük dosyaları yükleyemiyorum."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "@tiranozorbot'u kullandığınız için teşekkürler)"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "{} saniye içinde İndirildi.\n{} saniye içinde yüklendi.\n\n@tiranozorbot"
+    SAVED_CUSTOM_THUMB_NAIL = "✅ Video/Dosya için kapak fotoğrafı kaydedildi. Bu görüntü video/dosya için kullanılacaktır."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Kapak fotoğrafı başarıyla temizlendi."
     CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
-    HELP_USER = """How to Use Me? Follow These steps!
+    NO_VOID_FORMAT_FOUND = "HATA...\n<b>YouTubeDL</b>: {}"
+    HELP_USER = """Nasıl kullanılırım? Aşağıdaki adımları izleyin!
     
-1. Send url (example.domain/File.mp4 | New Filename.mp4).
-2. Send Image As Custom Thumbnail (Optional).
-3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File (video) as file with Screenshots
-   Video  - Give File as video without Screenshots
-   File   - Give File without Screenshots
-
-If bot didn't respond, contact @xgorn"""
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-You can use /rename command after receiving file to rename it with custom thumbnail support.
-"""
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+1. URL gönderin (örnek.domain/Dosya.mp4 | Yeni DosyaAdı.mp4).
+2. Kapak fotoğrafı için fotoğraf gönderin (İsteğe bağlı).
+3. Buton seçin.
+   SVideo - Ekran görüntüleri ile video olarak verir
+   DFile  - Ekran görüntüleri ile dosya olarak verir
+   Video  - Ekran görüntüsü olmadan video olarak verir
+   File   - Ekran görüntüsü olmadan dosya verir
+Bot cevap vermediyse @thebans ile iletişime geçin"""
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Kapak fotoğrafı oluşturmak için bir medya albümüne /genthumb komutunu yanıtlayın"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Albüm sadece iki fotoğraf içermelidir. Lütfen albümü yeniden gönderin ve tekrar deneyin veya bir albümde yalnızca iki adet fotoğraf gönderin."""
+    CANCEL_STR = "İşlem İptal Edildi"
+    ZIP_UPLOADED_STR = "{} Dosyası {} saniye içinde yüklendi"
+    SLOW_URL_DECED = "Tanrım, çok yavaş bir URL gibi görünüyor. Evimi mahvettiğin için bu dosyayı indirecek havamda değilim. Bu arada, neden bunu denemiyorsun?:==> https://shrtz.me/PtsVnf6 buradan bana hızlı bir URL alın, böylece yavaşlamadan Telegram'a yükleyebilirim."
